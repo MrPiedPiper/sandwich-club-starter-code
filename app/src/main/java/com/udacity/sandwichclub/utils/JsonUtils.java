eaced69b55dcd22b62460b5fa7cdd68b8be5e7fa
Napu Taitano <napukoconut@gmail.com>
2018-04-27 19:19:12 -0700

    Add parseSandwichJson functionality
    Located in utils/JsonUtils

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,68 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
+import com.udacity.sandwichclub.MainActivity;
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    //parseSandwichJson function returns a new Sandwich object with data parsed from JSON
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        //Create sandwich property variables
+        String mainName = null;
+        List<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin = null;
+        String description = null;
+        String image = null;
+        List<String> ingredients = new ArrayList<>();
+
+        //Try to get and set the properties from the JSON
+        try{
+            //Create a JSONObject from the json String
+            JSONObject sandwichJson = new JSONObject(json);
+
+            //Set mainName
+            mainName = sandwichJson.getJSONObject("name").getString("mainName");
+
+            //Get the alsoKnownAs property, then convert it into a List<String>, and set the alsoKnownAs variable
+            JSONArray tempAlsoKnownAs = sandwichJson.getJSONObject("name").getJSONArray("alsoKnownAs");
+            for(int i = 0; i < tempAlsoKnownAs.length(); i++){
+                alsoKnownAs.add(tempAlsoKnownAs.getString(i));
+            }
+
+            //Set placeOfOrigin
+            placeOfOrigin = sandwichJson.getString("placeOfOrigin");
+
+            //Set description
+            description = sandwichJson.getString("description");
+
+            //Set image
+            image = sandwichJson.getString("image");
+
+            //Get the ingredients property, then convert it into a List<String>, and set the alsoKnownAs variable
+            JSONArray tempIngredients = sandwichJson.getJSONArray("ingredients");
+            for(int i = 0; i < tempIngredients.length(); i++){
+                ingredients.add(tempIngredients.getString(i));
+            }
+        } catch (JSONException e) {
+            //If there's a problem, catch and log an error message
+            Log.e(MainActivity.class.getName(), "Error parsing JSON! : " + e.toString());
+            e.printStackTrace();
+        }
+
+        //Create a new Sandwich variable with the retrieved attributes
+        Sandwich returnSandwich = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        //Return the returnSandwich variable
+        return returnSandwich;
     }
 }
